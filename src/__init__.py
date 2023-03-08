3b9851bec8b86baa5ae19952bb8a0cffbaeb5865
Academich <mihandronov@gmail.com>
2023-03-08 20:48:13 +0100

    Imported here everything that is needed in main

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,8 @@
 from src.wrappers import Seq2SeqDM, TokenVocabulary
-from src.model.model import TextTranslationTransformer, BeamSearchDecoder
+from src.model.model import TextTranslationTransformer
+from src.model.model import VanillaTransformer
+from src.translation.translators import TranslationInferenceGreedy, TranslationInferenceBeamSearch
+
+available_models = {
+    "vanilla": VanillaTransformer
+}
