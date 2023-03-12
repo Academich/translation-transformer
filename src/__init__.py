2a0e60a9817ffbd2164f07cce71a3e91f394f456
Academich <mihandronov@gmail.com>
2023-03-12 22:11:06 +0100

    Oops a mistake

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,7 @@
+from src.lightning_module import TextTranslationTransformer
+from src.wrappers import CopySequence
 from src.model.model import VanillaTransformer
+
 
 model_catalogue = {
     "vanilla": VanillaTransformer
