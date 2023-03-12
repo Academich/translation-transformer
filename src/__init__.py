a05f4986eda8102d9ae88b13ec2607619ee9eb7b
Academich <mihandronov@gmail.com>
2023-03-12 22:09:51 +0100

    Updated the classes meant to be loaded into main

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,5 @@
-from src.wrappers import Seq2SeqDM, TokenVocabulary
-from src.model.model import TextTranslationTransformer
 from src.model.model import VanillaTransformer
-from src.translation.translators import TranslationInferenceGreedy, TranslationInferenceBeamSearch
 
-available_models = {
+model_catalogue = {
     "vanilla": VanillaTransformer
 }
