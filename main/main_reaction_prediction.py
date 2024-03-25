972534b1e6181d109dbfbb857fedac07b7060e4e
mikhailandronov <mihandronov@gmail.com>
2024-03-25 18:00:46 +0100

    Added scripts to train reaction prediction models

---
+++ main/main_reaction_prediction.py
@@ -0,0 +1,130 @@
+from pathlib import Path
+
+from pytorch_lightning.cli import LightningCLI
+
+from lightning_model_wrappers import VanillaTextTranslationTransformer
+from data_wrappers import Seq2SeqDM
+from tasks.reaction_prediction.tokenizer import ChemSMILESTokenizer
+
+from callbacks import PredictionWriter, DecodingCallback
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+
+
+class FlexibleCLI(LightningCLI):
+    """
+    A CLI that allows using subcommands together with run=False.
+    """
+
+    def add_arguments_to_parser(self, parser):
+        parser.add_argument(
+            "--subcmd",
+            "-s",
+            type=str,
+            choices=["fit", "validate", "test", "predict", "tune"],
+            required=True,
+            help="""
+            'fit' - Run the full optimization routine.
+            'validate' - Perform one evaluation epoch over the validation set.
+            'test' - Perform one evaluation epoch over the test set.
+            'predict' - Run inference on your data.
+            'tune' - Runs routines to tune hyperparameters before training."""
+        )
+        parser.add_argument("--ckpt_path", type=str, default=None,
+                            help="""
+            Path/URL of the checkpoint from which training is resumed.
+            Could also be one of two special keywords ``"last"`` and ``"hpc"``.
+            If there is no checkpoint file at the path, an exception is raised.
+            If resuming from mid-epoch checkpoint, training will start from the beginning of the next epoch.
+            (type: Union[str, null])""")
+
+        parser.add_argument("--src_vocab_path", type=str, default=None,
+                            help="""
+                    Path to the source sequence tokenizer vocabulary JSON""")
+
+        parser.add_argument("--tgt_vocab_path", type=str, default=None,
+                            help="""
+                    Path to the target sequence tokenizer vocabulary JSON""")
+
+        parser.add_argument("--write_predictions_path", type=str, default=None,
+                            help="""
+                            Path to the file to store the decoded predictions to""")
+
+    def before_instantiate_classes(self) -> None:
+        # Creating tokenizers
+        data_dir = Path(self.config.data.data_dir).resolve()
+        # Training a source tokenizer
+        src_tokenizer = ChemSMILESTokenizer()
+        if self.config.src_vocab_path is not None:
+            src_tokenizer.load_vocab(self.config.src_vocab_path)
+        else:
+            print("Training source tokenizer...")
+            src_tokenizer.train_tokenizer(data_dir / "src-train.txt")
+
+        # Training a target tokenizer
+        tgt_tokenizer = ChemSMILESTokenizer()
+        if self.config.tgt_vocab_path is not None:
+            tgt_tokenizer.load_vocab(self.config.tgt_vocab_path)
+        else:
+            print("Training target tokenizer...")
+            tgt_tokenizer.train_tokenizer(data_dir / "tgt-train.txt")
+
+        # Merging source and target tokenizer vocabularies
+        print("Merging source and target tokenizer vocabularies...")
+        merged_enc_dict = {**tgt_tokenizer.encoder_dict, **src_tokenizer.encoder_dict}
+        merged_tokenizer = ChemSMILESTokenizer()
+        merged_tokenizer.assign_vocab(merged_enc_dict)
+        save_vocab_dir = data_dir / "vocabs"
+        save_vocab_dir.mkdir(parents=True, exist_ok=True)
+        merged_tokenizer.save_vocab(save_vocab_dir / "vocab.json")
+
+        # Connecting the tokenizers to the model and the data module
+        self.config.data.src_tokenizer = merged_tokenizer
+        self.config.data.tgt_tokenizer = merged_tokenizer
+        self.config.model.src_vocab_size = merged_tokenizer.n_tokens
+        self.config.model.tgt_vocab_size = merged_tokenizer.n_tokens
+        self.config.model.pad_token_idx = merged_tokenizer.pad_token_idx
+        self.config.model.bos_token_idx = merged_tokenizer.bos_token_idx
+        self.config.model.eos_token_idx = merged_tokenizer.eos_token_idx
+
+        # Creating callbacks
+        if self.config.write_predictions_path is None:
+            self.config.write_predictions_path = "results/reaction_prediction_results.csv"
+        cb_list = [
+            PredictionWriter(self.config.write_predictions_path, merged_tokenizer),
+            DecodingCallback(merged_tokenizer),
+            LearningRateMonitor(logging_interval='step'),
+            ModelCheckpoint(
+                monitor="val/acc_single_tok",
+                auto_insert_metric_name=False,
+                filename='step={step}-val_tok_acc={val/acc_single_tok:.3f}-val_l={val/loss:.3f}',
+                mode='min',
+                save_last=False,
+                save_top_k=2,
+                every_n_epochs=1,
+                save_on_train_epoch_end=False,
+                enable_version_counter=False
+            )]
+
+        # Connecting callbacks to the trainer instance
+        if self.config.trainer.callbacks is None:
+            self.config.trainer.callbacks = []
+        for cb in cb_list:
+            self.config.trainer.callbacks.append(cb)
+
+
+if __name__ == '__main__':
+    cli = FlexibleCLI(
+        model_class=VanillaTextTranslationTransformer,
+        datamodule_class=Seq2SeqDM,
+        run=False,
+        save_config_callback=None,
+    )
+
+    if cli.config.subcmd == "fit":
+        cli.trainer.fit(cli.model, datamodule=cli.datamodule, ckpt_path=cli.config.ckpt_path)
+    if cli.config.subcmd == "validate":
+        cli.trainer.validate(cli.model, datamodule=cli.datamodule, ckpt_path=cli.config.ckpt_path)
+    if cli.config.subcmd == "test":
+        cli.trainer.test(cli.model, datamodule=cli.datamodule, ckpt_path=cli.config.ckpt_path)
+    if cli.config.subcmd == "predict":
+        cli.trainer.predict(cli.model, datamodule=cli.datamodule, ckpt_path=cli.config.ckpt_path)
---
+++ src/tasks/reaction_prediction/tokenizer.py
@@ -0,0 +1,42 @@
+import re
+from pathlib import Path
+from collections import Counter
+from itertools import chain
+
+from tokenization import GenericTokenizer, reverse_dict
+
+REGEX = "(\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\(|\)|\.|=|#|-|\+|\\\\|\/|:|~|@|\?|>|\*|\$|\%[0-9]{2}|[0-9])"
+
+
+class SimpleSmilesTokenizer:
+    pattern = re.compile(REGEX)
+
+    @classmethod
+    def split_into_tokens(cls, smi: str, check_reconstruction=False) -> list[str]:
+        tokens = [token for token in cls.pattern.findall(smi)]
+        if check_reconstruction:
+            assert smi == ''.join(tokens)
+        return tokens
+
+
+class ChemSMILESTokenizer(GenericTokenizer):
+
+    def train_tokenizer(self, train_data_path: Path | str) -> None:
+        with open(train_data_path) as f:
+            train_data = [line.strip() for line in f.readlines()]
+
+        tokenized_data = [SimpleSmilesTokenizer.split_into_tokens(line,
+                                                                  check_reconstruction=True) for line in train_data]
+        token_counts = Counter(chain(*tokenized_data))
+
+        for token, _ in token_counts.most_common():
+            self.encoder_dict[token] = len(self.encoder_dict)
+
+        self.decoder_dict = reverse_dict(self.encoder_dict)
+
+    def encode(self, seq: str) -> list[int]:
+        unk = self.encoder_dict[self.unk_token]
+        smi_tokens = SimpleSmilesTokenizer.split_into_tokens(seq, check_reconstruction=False)
+        token_ids = [self.encoder_dict[i] if i in self.encoder_dict else unk for i in smi_tokens]
+        token_ids = [self.bos_token_idx, *token_ids, self.eos_token_idx]
+        return token_ids
