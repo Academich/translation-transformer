6535ec08b31276f575ccaf9f28b973bd6230e980
mikhailandronov <mihandronov@gmail.com>
2024-04-19 15:11:12 +0200

    The tokenizer is the property of the lightning module

--- src/callbacks.py
+++ src/callbacks.py
@@ -7,10 +7,9 @@ from pytorch_lightning.callbacks import BasePredictionWriter
 
 
 class DecodingCallback(Callback):
-    def __init__(self, tgt_tokenizer):
+    def __init__(self):
         super().__init__()
         self.validation_step_outputs = []
-        self.tkz = tgt_tokenizer
 
     def on_validation_batch_end(
             self,
@@ -24,14 +23,15 @@ class DecodingCallback(Callback):
         self.validation_step_outputs.append(outputs)
 
     def on_validation_epoch_end(self, trainer: "Trainer", pl_module: "LightningModule") -> None:
+        tkz = pl_module.tgt_tokenizer
         total_correct, total = 0, 0
         for o in self.validation_step_outputs:
             pred_tokens = o["pred_tokens"].cpu().numpy()
             target_ahead = o["target_ahead"].cpu().numpy()
             b_size = pred_tokens.shape[0]
             for i in range(b_size):
-                target_str = self.tkz.decode(target_ahead[i])
-                predicted_str = self.tkz.decode(pred_tokens[i])
+                target_str = tkz.decode(target_ahead[i])
+                predicted_str = tkz.decode(pred_tokens[i])
                 total_correct += int(predicted_str == target_str)
                 total += 1
         trainer.logger.log_metrics({"val/whole_seq_exact_match_acc_total": total_correct / total})
@@ -40,20 +40,20 @@ class DecodingCallback(Callback):
 
 class PredictionWriter(BasePredictionWriter):
 
-    def __init__(self, output_dir, tgt_tokenizer, write_interval='batch'):
+    def __init__(self, output_dir, write_interval='batch'):
         super().__init__(write_interval)
         self.output_path = Path(output_dir).resolve()
         self.output_path.unlink(missing_ok=True)
         self.output_path.parent.mkdir(exist_ok=True)
-        self.tkz = tgt_tokenizer
 
     def write_on_batch_end(
             self, trainer, pl_module, prediction, batch_indices, batch, batch_idx, dataloader_idx
     ):
+        tkz = pl_module.tgt_tokenizer
         with open(self.output_path, "a") as f:
             tgt = batch["tgt_tokens"].cpu().numpy()
             pred = prediction.cpu().numpy()
             for i, t in enumerate(tgt):
-                t_string = self.tkz.decode(t)
-                p_options = self.tkz.decode_batch(pred[i])
+                t_string = tkz.decode(t)
+                p_options = tkz.decode_batch(pred[i])
                 print(",".join([t_string] + p_options), file=f)
