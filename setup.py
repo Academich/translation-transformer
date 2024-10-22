96f0f3d6acf3f952fb0aa3794aae1db49a11ae1d
Academich <mihandronov@gmail.com>
2024-10-22 09:57:16 +0100

    Added arguments to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup()
+    setup(
+        name='molecular_transformer_lightning',
+        version='0.1.0',
+        description='SMILES-to-SMILES Transformer for chemical reactions',
+        author='Mikhail Andronov',
+        packages=['molecular_transformer_lightning'],
+        install_requires=[
+            'lightning',
+            'jsonargparse[signatures]',
+            'rdkit',
+            'tensorboard',
+            'gdown',
+        ],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.10',
+        ],
+    )
