d0f16b948867a91c76874d952ca1ba38a84caef5
Academich <mihandronov@gmail.com>
2024-12-30 15:00:54 +0000

    Added experiments with smart drafting to retrosynthesis

--- scripts/single_step_retrosynthesis.sh
+++ scripts/single_step_retrosynthesis.sh
@@ -53,11 +53,12 @@ function run_speculative_beam_search() {
   local NBEST=${3:-5} # Number of best sequences
   local DRAFT_LEN=${4:-10} # Draft sequence length
   local N_DRAFTS=${5:-23} # Maximum number of parallel drafts 
-  local GPU=${6:-1}
-  local SAVE_PREDICTIONS=${7:-false} # Whether to save predictions to disk. Slows down the run.
+  local SMART_DRAFTS_MODE=${6:-1}
+  local GPU=${7:-0}
+  local SAVE_PREDICTIONS=${8:-false} # Whether to save predictions to disk. Slows down the run.
 
   local DEVICE="--trainer.accelerator cpu --trainer.devices 1"
-  if [ -n "${GPU}" ]; then
+  if [ "${GPU}" != "false" ]; then
     DEVICE="--trainer.accelerator gpu --trainer.devices [${GPU}]"
   fi
 
@@ -79,6 +80,7 @@ function run_speculative_beam_search() {
           --model.report_prediction_file ${OUTPUT_DIR}/report.txt \
           --data.batch_size ${BS} \
           --model.generation beam_search_speculative \
+          --model.smart_drafts_mode ${SMART_DRAFTS_MODE} \
           --model.draft_len ${DRAFT_LEN} \
           --model.beam_size ${NBEST} \
           --model.max_len ${MAX_LEN} \
@@ -90,6 +92,7 @@ function run_speculative_beam_search() {
 GPU=0
 BATCH_SIZE=1
 SAVE_PREDICTIONS=false
+SMART_DRAFTS=false
 for i in {1..6}; do
 
   # 5 beams, 11 draft tokens, 15 drafts
@@ -97,28 +100,28 @@ for i in {1..6}; do
   draft_len=11
   n_drafts=15
   run_beam_search results_retrosynthesis_beam_search_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${GPU} ${SAVE_PREDICTIONS}
-  run_speculative_beam_search results_retrosynthesis_sbs_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${draft_len} ${n_drafts} ${GPU} ${SAVE_PREDICTIONS}
+  run_speculative_beam_search results_retrosynthesis_sbs_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
 
   # 10 beams, 10 draft tokens, 10 drafts
   n_best=10
   draft_len=10
   n_drafts=10
   run_beam_search results_retrosynthesis_beam_search_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${GPU} ${SAVE_PREDICTIONS}
-  run_speculative_beam_search results_retrosynthesis_sbs_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${draft_len} ${n_drafts} ${GPU} ${SAVE_PREDICTIONS}
+  run_speculative_beam_search results_retrosynthesis_sbs_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
 
   # 15 beams, 10 draft tokens, 10 drafts
   n_best=15
   draft_len=10
   n_drafts=10
   run_beam_search results_retrosynthesis_beam_search_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${GPU} ${SAVE_PREDICTIONS}
-  run_speculative_beam_search results_retrosynthesis_sbs_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${draft_len} ${n_drafts} ${GPU} ${SAVE_PREDICTIONS}
+  run_speculative_beam_search results_retrosynthesis_sbs_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
 
   # 20 beams, 14 draft tokens, 5 drafts
   n_best=20
   draft_len=14
   n_drafts=5
   run_beam_search results_retrosynthesis_beam_search_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${GPU} ${SAVE_PREDICTIONS}
-  run_speculative_beam_search results_retrosynthesis_sbs_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${draft_len} ${n_drafts} ${GPU} ${SAVE_PREDICTIONS}
+  run_speculative_beam_search results_retrosynthesis_sbs_bs_${BATCH_SIZE}_nbest_${n_best}_gpu${GPU} ${BATCH_SIZE} ${n_best} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
 
   if [ "$i" -eq 5 ]; then
     SAVE_PREDICTIONS=true
@@ -128,30 +131,47 @@ done
 
 # Beam search decoding with ten hypotheses
 # Five runs for time spread estimation
+GPU=0
 N_BEST=10
 SAVE_PREDICTIONS=false
 for i in {1..6}; do
+
+  # batch size 1, 10 draft tokens, 10 drafts
+  batch_size=1
+  draft_len=10
+  n_drafts=10
+  SMART_DRAFTS=true
+  run_speculative_beam_search results_retrosynthesis_sbs_smart_drafts_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
 
   # batch size 2, 12 draft tokens, 5 drafts
   batch_size=2
   draft_len=12
   n_drafts=5
   run_beam_search results_retrosynthesis_beam_search_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${GPU} ${SAVE_PREDICTIONS}
-  run_speculative_beam_search results_retrosynthesis_sbs_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${GPU} ${SAVE_PREDICTIONS}
+  SMART_DRAFTS=false
+  run_speculative_beam_search results_retrosynthesis_sbs_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
+  SMART_DRAFTS=true
+  run_speculative_beam_search results_retrosynthesis_sbs_smart_drafts_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
 
   # batch size 4, 10 draft tokens, 3 drafts
   batch_size=4
   draft_len=10
   n_drafts=3
   run_beam_search results_retrosynthesis_beam_search_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${GPU} ${SAVE_PREDICTIONS}
-  run_speculative_beam_search results_retrosynthesis_sbs_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${GPU} ${SAVE_PREDICTIONS}
+  SMART_DRAFTS=false
+  run_speculative_beam_search results_retrosynthesis_sbs_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
+  SMART_DRAFTS=true
+  run_speculative_beam_search results_retrosynthesis_sbs_smart_drafts_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
 
   # batch size 8, 10 draft tokens, 2 drafts
   batch_size=8
   draft_len=10
   n_drafts=2
   run_beam_search results_retrosynthesis_beam_search_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${GPU} ${SAVE_PREDICTIONS}
-  run_speculative_beam_search results_retrosynthesis_sbs_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${GPU} ${SAVE_PREDICTIONS}
+  SMART_DRAFTS=false
+  run_speculative_beam_search results_retrosynthesis_sbs_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
+  SMART_DRAFTS=true
+  run_speculative_beam_search results_retrosynthesis_sbs_smart_drafts_bs_${batch_size}_nbest_${N_BEST}_gpu${GPU} ${batch_size} ${N_BEST} ${draft_len} ${n_drafts} ${SMART_DRAFTS} ${GPU} ${SAVE_PREDICTIONS}
 
   if [ "$i" -eq 5 ]; then
     SAVE_PREDICTIONS=true
